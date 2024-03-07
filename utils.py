1805bd970d1ee06eb26afab488e01ed8880df891
Pavanpreet Singh Gandhi <pavanpreet.gandhi@gmail.com>
2024-03-07 22:36:50 +0000

    Include requirements.txt and make compatible with unix filepath format

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import yaml
 from munch import munchify
 
 def load_sura():
-    filepath = '.\sura.yml'
+    filepath = './sura.yml'
     with open(filepath, 'r') as file:
         sura = yaml.safe_load(file)
         sura = munchify(sura)
