835fdb25a8903ed341307b326ad2edb1ccbaa5d0
Pavanpreet Singh Gandhi <pavanpreet.gandhi@gmail.com>
2024-03-24 12:36:03 +0000

    Refactor Raag class to use __contains__ method for checking Note and Chord objects in raag, aroh, and avroh

--- music_elements/raag.py
+++ music_elements/raag.py
@@ -22,79 +22,55 @@ class Raag:
         self.avroh = [Note(note) if isinstance(note, (str, int)) else note for note in avroh]
 
 
-    def is_note_in_raag(self, note: Note) -> bool:
+    def __contains__(self, item: Union[Note, Chord]) -> bool:
         """
-        Checks if a given note is in the raag.
+        Checks if a given Note or Chord object is in the raag.
 
         Args:
-            note ('Note'): The Note object to check.
+            item (Union[Note, Chord]): The Note or Chord object to check.
 
         Returns:
-            bool: True if the note is in the raag, False otherwise.
+            bool: True if the Note or Chord object is in the raag, False otherwise.
         """
-        return note.get_base_note() in self.notes
+        if isinstance(item, Note):
+            return item.get_base_note() in self.notes
+        elif isinstance(item, Chord):
+            return all(note.get_base_note() in self.notes for note in item.notes)
+        else:
+            return False
     
     
-    def is_note_in_aroh(self, note: Note) -> bool:
+    def aroh_contains(self, item: Union[Note, Chord]) -> bool:
         """
-        Checks if a given note is in the aroh of the raag.
+        Checks if a given Note or Chord object is in the aroh of the raag.
 
         Args:
-            note ('Note'): The Note object to check.
+            item (Union[Note, Chord]): The Note or Chord object to check.
 
         Returns:
-            bool: True if the note is in the aroh, False otherwise.
+            bool: True if the Note or Chord object is in the aroh of the raag, False otherwise.
         """
-        return note.get_base_note() in self.aroh
+        if isinstance(item, Note):
+            return item.get_base_note() in self.aroh
+        elif isinstance(item, Chord):
+            return all(note.get_base_note() in self.aroh for note in item.notes)
+        else:
+            return False
     
     
-    def is_note_in_avroh(self, note: Note) -> bool:
+    def avroh_contains(self, item: Union[Note, Chord]) -> bool:
         """
-        Checks if a given note is in the avroh of the raag.
+        Checks if a given Note or Chord object is in the avroh of the raag.
 
         Args:
-            note ('Note'): The Note object to check.
+            item (Union[Note, Chord]): The Note or Chord object to check.
 
         Returns:
-            bool: True if the note is in the avroh, False otherwise.
+            bool: True if the Note or Chord object is in the avroh of the raag, False otherwise.
         """
-        return note.get_base_note() in self.avroh
-
-
-    def is_chord_in_raag(self, chord: Chord) -> bool:
-        """
-        Checks if a given chord is in the raag.
-
-        Args:
-            chord (Chord): The Chord object to check.
-
-        Returns:
-            bool: True if the chord is in the raag, False otherwise.
-        """
-        return all(note.get_base_note() in self.notes for note in chord.notes)
-    
-    
-    def is_chord_in_aroh(self, chord: Chord) -> bool:
-        """
-        Checks if a given chord is in the aroh of the raag.
-
-        Args:
-            chord (Chord): The Chord object to check.
-
-        Returns:
-            bool: True if the chord is in the aroh, False otherwise.
-        """
-        return all(note.get_base_note() in self.aroh for note in chord.notes)
-    
-    
-    def is_chord_in_avroh(self, chord: Chord) -> bool:
-        """
-        Checks if a given chord is in the avroh of the raag.
-
-        Args:
-            chord (Chord): The Chord object to check.
-
-        Returns:
-            bool: True if the chord is in the avroh, False otherwise.
-        """
-        return all(note.get_base_note() in self.avroh for note in chord.notes)
+        if isinstance(item, Note):
+            return item.get_base_note() in self.avroh
+        elif isinstance(item, Chord):
+            return all(note.get_base_note() in self.avroh for note in item.notes)
+        else:
+            return False
